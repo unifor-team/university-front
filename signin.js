4856fb48aeb5c071502320d241fc0e1797eca204
Manelito <emanuelwork1@gmail.com>
2023-11-16 20:45:01 -0300

    feat: adjust on home page and singup toastify loading

--- signin.js
+++ signin.js
@@ -1,27 +1,27 @@
 const url = 'http://localhost:8080/api/v1/login'
 
-if(localStorage.getItem("token")){
-    window.location.replace("index.html");
-  }
-async function addNewUser(){
-        userData = {
-        "email": document.querySelector("#email").value,
-        "password": document.querySelector("#password").value
-     }
-     if(userData.email == ""){
-        alert('Campo Email é obrigatório!')
-     }
-     if(userData.password == ""){
-        alert('Campo Password é obrigatório!')
-     }
-    await axios.post(url, userData)
-    .then(response =>{
-        console.log(response.data);
-        window.location.replace("index.html")
-        localStorage.setItem("token", JSON.stringify(response.data))
-    })
-    .catch(error => alert('usuário ou senha incorreto!'));
-    console.log(teste);
-    
+if (localStorage.getItem("token")) {
+   window.location.replace("index.html");
+}
+
+async function addNewUser() {
+   userData = {
+      "email": document.querySelector("#email").value,
+      "password": document.querySelector("#password").value
+   }
+   if (userData.email == "") {
+      alert('Campo Email é obrigatório!')
+   }
+   if (userData.password == "") {
+      alert('Campo Password é obrigatório!')
+   }
+   await axios.post(url, userData)
+      .then((response) => {
+         localStorage.setItem("token", JSON.stringify(response.data.token))
+         setTimeout(() => {
+            window.location.replace("index.html");
+         }, 2000);
+      })
+      .catch(error => alert('usuário ou senha incorreto!'));
 }
 
--- signup.js
+++ signup.js
@@ -1,29 +1,63 @@
 const url = 'http://localhost:8080/api/v1/register'
 
- if(localStorage.getItem("token")){
+if (localStorage.getItem("token")) {
    window.location.replace("index.html");
- }
-async function addNewUser(){
-        newUser = {
-        "email": document.querySelector("#email").value,
-        "password": document.querySelector("#password").value,
-        "name": document.querySelector("#name").value,
-        "role": "USER"
-     }
-     if(newUser.email == ""){
-        alert('Campo Email é obrigatório!')
-     }
-     if(newUser.password == ""){
-        alert('Campo Password é obrigatório!')
-     }
-     if(newUser.name == ""){
-        alert('Campo Nome é obrigatório!')
-     }
-    await axios.post(url, newUser)
-    .then(response =>{
-        alert('usuário cadastrado!');
-        window.location.replace("signin.html")
-    })
-    .catch(error => console.log(error));
+}
+
+const renderToastify = (text) => Toastify({
+   text,
+   duration: 3000,
+   newWindow: true,
+   close: true,
+   gravity: "top",
+   position: "right",
+   stopOnFocus: true,
+   style: {
+     background: "linear-gradient(to right, #00b09b, #96c93d)",
+   },
+   onClick: function(){}
+ }).showToast();
+
+
+async function addNewUser() {
+   const signupButton = document.querySelector(".button-signup");
+   const loadDiv = document.createElement("div");
+   const signupText = document.querySelector(".signup-text");
+
+   loadDiv.classList.add("lds-facebook");
+   loadDiv.appendChild(document.createElement("div"));
+   loadDiv.appendChild(document.createElement("div"));
+   loadDiv.appendChild(document.createElement("div"));
+
+   signupButton.removeChild(signupText);
+   signupButton.appendChild(loadDiv);
+
+
+   const newUser = {
+      "email": document.querySelector("#email").value,
+      "password": document.querySelector("#password").value,
+      "name": document.querySelector("#name").value,
+      "role": "USER"
+   }
+
+   // if (!newUser.email) alert('Campo Email é obrigatório!')
+   // if (!newUser.password) alert('Campo Password é obrigatório!')
+   // if (!newUser.name) alert('Campo Nome é obrigatório!')
+
+   try {
+      await axios.post(url, newUser);
+      renderToastify("Cadastro efetuado com sucesso.");
+      setTimeout(() => {
+         window.location.replace("signin.html");
+      }, 1000);
+   } catch (e) {
+      console.log(e);
+      renderToastify(e.message);
+
+   } finally {
+      signupButton.appendChild(signupText);
+      signupButton.removeChild(loadDiv);
+
+   }
 }
 
