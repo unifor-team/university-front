58cb37fd437f9a05775b32b7c9a4152723641f6c
Manelito <emanuelwork1@gmail.com>
2023-11-20 18:40:06 -0300

    refactor: using try catch instead then

--- signin.js
+++ signin.js
@@ -1,62 +1,58 @@
-const url = 'http://localhost:8080/api/v1/login'
+const url = "http://localhost:8080/api/v1/login";
 
 if (localStorage.getItem("token")) {
-   window.location.replace("index.html");
+  window.location.replace("index.html");
 }
 
-const renderToastify = (text) => Toastify({
-   text,
-   duration: 3000,
-   newWindow: true,
-   close: true,
-   gravity: "top",
-   position: "right",
-   stopOnFocus: true,
-   style: {
-     background: "linear-gradient(to right, #00b09b, #96c93d)",
-   },
-   onClick: function(){}
- }).showToast();
+const renderToastify = text =>
+  Toastify({
+    text,
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)"
+    },
+    onClick: function () {}
+  }).showToast();
 
+async function addNewUser() {
+  const signinButton = document.querySelector(".button-login");
+  const loadDiv = document.createElement("div");
+  const signinText = document.querySelector(".signup-text");
 
-async function addNewUser() { 
+  loadDiv.classList.add("lds-facebook");
+  loadDiv.appendChild(document.createElement("div"));
+  loadDiv.appendChild(document.createElement("div"));
+  loadDiv.appendChild(document.createElement("div"));
 
-   const signinButton = document.querySelector(".button-login");
-   const loadDiv = document.createElement("div");
-   const signinText = document.querySelector(".signup-text");
-
-   loadDiv.classList.add("lds-facebook");
-   loadDiv.appendChild(document.createElement("div"));
-   loadDiv.appendChild(document.createElement("div"));
-   loadDiv.appendChild(document.createElement("div"));
-
-   signinButton.removeChild(signinText);
-   signinButton.appendChild(loadDiv);
+  signinButton.removeChild(signinText);
+  signinButton.appendChild(loadDiv);
 
   userData = {
-      "email": document.querySelector("#email").value,
-      "password": document.querySelector("#password").value
-   }
-   if (userData.email == "" || userData.password == "" ) {
-      renderToastify("Email ou senha inválida")
-   }
+    email: document.querySelector("#email").value,
+    password: document.querySelector("#password").value
+  };
 
+  if (userData.email == "" || userData.password == "") {
+    renderToastify("Email ou senha inválida");
+  }
 
-   try{
-   await axios.post(url, userData)
-      .then((response) => {
-         localStorage.setItem("token", JSON.stringify(response.data.token))
-         setTimeout(() => {
-            window.location.replace("index.html");
-         }, 2000);
-      })
-   }catch(e ){
-      console.log(e.message)
-   }finally {
-      signinButton.appendChild(signinText);
-      signinButton.removeChild(loadDiv);
-   }
+  try {
+    const response = await axios.post(url, userData);
+    renderToastify("Login efetuado com sucesso");
+    localStorage.setItem("token", JSON.stringify(response.data.token));
+    setTimeout(() => {
+      window.location.replace("index.html");
+    }, 2000);
+  } catch (e) {
+    renderToastify("Credenciais incorretas");
+    console.log(e.message);
+  } finally {
+    signinButton.appendChild(signinText);
+    signinButton.removeChild(loadDiv);
+  }
 }
-
-
-
