a32f7c7a292983c4533ab24ac3fccc84693de888
MatheusPonte <Matheuspontes894@outlook.com>
2023-11-21 20:48:54 -0300

    ajustes na tela de gestao adiministrativa

--- admscreen.js
+++ admscreen.js
@@ -3,6 +3,53 @@ const url = "http://localhost:8080/api/v1/user";
 let users;
 let currentId;
 
+const token = localStorage.getItem("token");
+
+if (token) {
+  const decodedToken = jwtDecode(token);
+  const username = document.querySelector(".user-name");
+  username.innerHTML = decodedToken.Name;
+  const divError = createElement("div", "divError")
+  const errorMessage = createElement("h1", "errorMessage")
+  const body = document.querySelector(".adm-page")
+  body.appendChild(divError)
+  errorMessage.innerHTML = "PAGE NOT FOUND - 404"
+  divError.appendChild(errorMessage)
+
+
+  
+
+}
+
+const renderToastify = text =>
+  Toastify({
+    text,
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)"
+    },
+    onClick: function () {}
+  }).showToast();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load',() => {
+      setTimeout(()=>{
+        preloader.remove();
+      }, 1000)
+    });
+  }
+})
+
 async function deleteUser() {
   const token = localStorage.getItem("token");
   const headers = {
@@ -11,11 +58,12 @@ async function deleteUser() {
 
   try {
     await axios.delete(`${url}/${currentId}`, { headers: headers });
+    renderToastify("Deletado com sucesso!");
     setTimeout(async () => {
       const modalContainer = document.querySelector(".modal-container");
       modalContainer.classList.add("modal-none");
       window.location.reload();
-    }, 500);
+    }, 1500);
   } catch (err) {
     console.log(err);
   }
@@ -45,6 +93,15 @@ async function getAllUsers() {
 
   users = (await axios.get(url, { headers: headers })).data;
   listUsersOnScreen();
+  
+}
+window.onload = getAllUsers();
+ 
+
+
+function closeModal(){
+  const modalContainer = document.querySelector(".modal-container");
+  modalContainer.classList.add("modal-none")
 }
 
 function listUsersOnScreen() {
